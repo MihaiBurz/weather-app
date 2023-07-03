63296986fb0b0c596c2e9cdb5842250791348af9
MihaiBurz <mihai.burz23@gmail.com>
2023-07-03 16:12:35 +0300

    Style

--- sections/weatherForecast.js
+++ sections/weatherForecast.js
@@ -6,24 +6,47 @@ function displayWeatherForecast(city) {
             console.log(data);
             const { list } = data;
             const weatherForecast = document.querySelector('.weather-forecast');
-            weatherForecast.innerHTML = '';
-            list.forEach((forecast) =>{
-                const {dt, main, weather} = forecast;
-                const hour = getHours(dt);
+            const days = {};
+            weatherForecast.innerHTML += ``;
+
+            list.forEach((forecast) => {
+                const { dt } = forecast;
                 const day = getDay(dt);
-                const iconUrl = getIconUrl(weather[0].icon);
-                const temperature = Math.round(main.temp);
-                const weatherDescription = weather[0].description;
-                const realFeel = Math.round(main.feels_like);
-                weatherForecast.innerHTML += `
-                <div class="d-flex justify-content-between">
-                    <p>${day} , ${hour}</p>
-                    <img src="${iconUrl}" />
-                    <p>${temperature}</p>
-                    <p>${weatherDescription}</p>
-                    <p> Real Feel: ${realFeel} </p>
-                </div>
-                `
+                if(days[day] === undefined) 
+                {
+                    days[day] = [forecast];
+                }
+                else {
+                    days[day].push(forecast);
+                }
             })
-        })
-}
+
+            for (const day in days) {
+                console.log(day);
+                console.log(days[day]);
+                weatherForecast.innerHTML += `
+                <h2>${day}<h2>
+                `;
+
+                const list = days[day];
+                list.forEach((forecast) =>{
+                        const {dt, main, weather} = forecast;
+                        const hour = getHours(dt);
+                        const day = getDay(dt);
+                        const iconUrl = getIconUrl(weather[0].icon);
+                        const temperature = Math.round(main.temp);``
+                        const weatherDescription = weather[0].description;
+                        const realFeel = Math.round(main.feels_like);
+                        weatherForecast.innerHTML += `
+                        <div class="weather-forecast-box d-flex justify-content-between border rounded p-3 mb-3 align-items-center">
+                            <p>${hour}</p>
+                            <div><img src="${iconUrl}" /></div>
+                            <p><strong>${temperature}°C</strong></p>
+                            <p>${weatherDescription}</p>
+                            <p class="real-feel"> Real Feel: <strong> ${realFeel}°C </strong></p>
+                        </div>`
+                    });
+
+                }
+            }
+)};
